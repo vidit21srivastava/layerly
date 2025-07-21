542e7ad22f7c5fa6013b5fe5a65ca0bb4f16c038
vidit21srivastava <vidit21srivastava@gmail.com>
2025-07-22 02:48:00 +0530

    Minor Commit

--- frontend/src/components/NewLaunch.jsx
+++ frontend/src/components/NewLaunch.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const NewLaunch = () => {
     console.log('NewLaunch component rendered');
     const { products } = useContext(ShopContext);
-    console.log(products);
+
     return (
-        <div></div>
+        <div className='my-10'>
+            <div className='text-left py-8 text-3x1'>
+                <Title text1={'Newly '} text2={'Launched'} />
+            </div>
+        </div>
     )
 }
 
