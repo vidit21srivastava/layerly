b402f60108dc66969b8f51be11824e172b8af582
vidit21srivastava <vidit21srivastava@gmail.com>
2025-09-30 02:46:54 +0530

    Minor Commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -105,7 +105,7 @@ const Footer = () => {
                 </span>
                 <span>
                   <strong>
-                    Layerly <br />
+                    Layerly
                   </strong>
                   <br />
                   Ranchhodray Nagar Society,
